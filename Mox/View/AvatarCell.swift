9de8025be1df17b1eb6fa4e1b7c57c417d23817a
Mox <earl.ledesma@mycit.ie>
2018-05-06 19:30:54 +0100

    Added AvatarCells,PickerVC and Cell View for Main Storyboard.

---
+++ Mox/View/AvatarCell.swift
@@ -0,0 +1,29 @@
+//
+//  AvatarCell.swift
+//  Mox
+//
+//  Created by Earl Ledesma on 06/05/2018.
+//  Copyright © 2018 moxxvondee. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    
+}
