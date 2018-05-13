8dc3714c2d967a295468602302e4c94439a4f332
Mox <earl.ledesma@mycit.ie>
2018-05-13 10:36:36 +0100

    Added the timestamp label for each messages sent on each channels created via MessageCell.swift.

--- Mox/View/MessageCell.swift
+++ Mox/View/MessageCell.swift
@@ -28,6 +28,26 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        //2017-07-13T21:49:25.590Z  <-- This is the standard ISO 8691.
+        
+        // This is how to set the timestamp for the messages. (ie May 12-13, 9:43am)
+        
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy:  -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
+        
     }
 
 }
